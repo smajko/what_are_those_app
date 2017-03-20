9641a96e83376405d140b8d46ed05b05e24bc584
Sebastian Majko <sebastian.majko63@myhunter.cuny.edu>
2017-03-20 13:38:05 -0400

    add static async http client

--- app/src/main/java/org/smajko/watapp/MainActivity.java
+++ app/src/main/java/org/smajko/watapp/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends Activity {
 		}
 	}
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -106,6 +105,46 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				Toast.makeText(MainActivity.this, "Endpoint not implemented",
 						Toast.LENGTH_LONG).show();
+				/***************************************************************
+				 *                    TO BE IMPLEMENTED
+				 *
+
+				 RequestParams params = new RequestParams();
+
+				 params.put("symptoms", symptoms);
+				 params.put("gender", gender);
+				 params.put("age",age);
+				 params.put("days",days);
+				 params.put("reoccurring",reoccurring);
+
+				 try {
+				 	outputFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/myimage.png";
+				 	File myFile = new File(outputFilePath);
+				 	params.put("picture", myFile);
+				 } catch(FileNotFoundException e) {}
+
+				 HttpUtils.post(AppConstant.URL_FEED, params, new JsonHttpResponseHandler() {
+					@Override
+					public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+						// If the response is JSONObject instead of expected JSONArray
+						Log.d("res", "---------------- response : " + response);
+						try {
+							JSONObject serverResp = new JSONObject(response.toString());
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+
+					@Override
+					public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+						// Pull out the first event on the public timeline
+					}
+				});
+
+				 *
+				 *
+				 *****************************************************************/
 			}
 		});
 	}
