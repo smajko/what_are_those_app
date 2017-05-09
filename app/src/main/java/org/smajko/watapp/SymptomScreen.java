120017361598b31e1bf0a1a6a525193feedb8140
Sebastian Majko <sebastian.majko63@myhunter.cuny.edu>
2017-05-08 21:09:09 -0400

    add checkbox listeners

--- app/src/main/java/org/smajko/watapp/SymptomScreen.java
+++ app/src/main/java/org/smajko/watapp/SymptomScreen.java
@@ -5,20 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Button;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Toast;
 
+
 public class SymptomScreen extends Activity {
+    CheckBox cb;
     CheckBox[] sens = new CheckBox[7];
     CheckBox[] cols = new CheckBox[6];
     CheckBox[] oths = new CheckBox[19];
+
+    CheckBox[][] checkboxes = {sens, cols, oths};
 
     String sensations[] = {"pain", "itchiness", "numbness", "sore", "sensitive", "burning", "tingling"};
     String colors[] = {"redness", "brown", "fleshy", "white", "pink", "tan"};
     String misc[] = {"pus", "fever", "pimples", "whiteheads", "blackheads", "oval-shaped", "scabs", "mole", "lesion",
             "irregular-shape", "grainy", "swelling", "scales", "sores", "difficulty breathing", "wheezing", "blister", "sore throat", "bumps"};
+
+    String[][] strings = {sensations, colors, misc};
 
     String acne[] = {"whiteheads", "blackheads", "redness", "bumps", "pimples", "pain", "pus"};
     String hives[] = {"wheezing", "difficulty breathing", "swelling", "redness", "oval-shaped", "itchiness"};
@@ -30,6 +37,8 @@ public class SymptomScreen extends Activity {
     String poisonivyrash[] = {"redness", "itchiness", "swelling", "blister"};
     String scabies[] = {"itchiness", "blister", "scales", "sores"};
 
+    String[][] conditions = {acne, hives, shingles, chickenpox, skincancer, coldsore, wart, poisonivyrash, scabies};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +49,8 @@ public class SymptomScreen extends Activity {
         final LinearLayout colorsLayout = (LinearLayout) findViewById(R.id.colors);
         final LinearLayout othersLayout = (LinearLayout) findViewById(R.id.others);
 
+        LinearLayout[] layouts = {sensationsLayout, colorsLayout, othersLayout};
+
         final Button finish = (Button) findViewById(R.id.btnDone);
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,27 +59,27 @@ public class SymptomScreen extends Activity {
             }
         });
 
-        //LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(200,200);
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < checkboxes.length; i++)
         {
-            CheckBox cb = new CheckBox(this);
-            cb.setText(sensations[i]);
-            sens[i] = cb;
-            sensationsLayout.addView(sens[i]);
-        }
-        for (int i = 0; i < 6; i++)
-        {
-            CheckBox cb = new CheckBox(this);
-            cb.setText(colors[i]);
-            cols[i] = cb;
-            colorsLayout.addView(cols[i]);
-        }
-        for (int i = 0; i < 19; i++)
-        {
-            CheckBox cb = new CheckBox(this);
-            cb.setText(misc[i]);
-            oths[i] = cb;
-            othersLayout.addView(oths[i]);
+            for (int j = 0; j < checkboxes[i].length; j++)
+            {
+                cb = new CheckBox(this);
+                cb.setText(strings[i][j]);
+                cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                    @Override
+                    public void onCheckedChanged(final CompoundButton buttonView, final boolean isChecked) {
+                        // TODO Auto-generated method stub
+                        if (isChecked) {
+                            System.out.println("Checked");
+                        } else {
+                            System.out.println("Un-Checked");
+                        }
+                    }
+                });
+                checkboxes[i][j] = cb;
+                layouts[i].addView(checkboxes[i][j]);
+            }
         }
     }
 
